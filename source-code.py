bdc9f5f6175edb57eba747cfce0cd89f83792926
farhatizakaria <zakariafarhati18@gmail.com>
2019-12-06 12:26:34 +0000

    Creat source-code.py
    Creat the main source code!

---
+++ source-code.py
@@ -0,0 +1,9 @@
+string = str(input("Write any string: "))
+count = 0
+#x = len(string)
+for letter in range(len(string)):
+    count = count + 1
+    print(string[count*-1])
+    # The end of "for" loop
+str(input("Press enter to exit "))
+exit()
