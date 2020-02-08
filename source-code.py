8fafef6c5d72a86e9d67ff26e44afa5888f37fe4
Zakaria Farhati <zakariafarhati18@gmail.com>
2020-02-08 17:47:02 +0100

    Update the source code

--- source-code.py
+++ source-code.py
@@ -1,9 +1,12 @@
-string = str(input("Write any string: "))
-count = 0
-#x = len(string)
-for letter in range(len(string)):
-    count = count + 1
-    print(string[count*-1])
-    # The end of "for" loop
-str(input("Press enter to exit "))
-exit()
+def main():
+    string = str(input("Write any string: "))
+    count = 0
+    #x = len(string)
+    for letter in range(len(string)):
+        count = count + 1
+        print(string[count*-1])
+        # The end of "for" loop
+    str(input("Press enter to exit "))
+    exit()
+if __name__ == "__main__":
+    main()
